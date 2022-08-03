7a4e114056c00f5b8dd430cc8f02d133375bea5f
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2022-08-03 09:04:42 +0600

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import AppHelmet from '../components/AppLayouts/AppHelmet/AppHelmet';
-import { ThemeSettings as TSettings } from '../components/AppModules/GlobalSettings/ThemeSettings';
 
 const Dashboard = () => {
 	return (
 		<>
 			<AppHelmet title="Dashboard" />
 			<h1 className="display-5 text-theme-color">Hello World !</h1>
-			<div className="row">
-				<div className="col-xxl-5 col-xl-6 col-lg-8">
-					<TSettings />
-				</div>
-			</div>
 		</>
 	);
 };
