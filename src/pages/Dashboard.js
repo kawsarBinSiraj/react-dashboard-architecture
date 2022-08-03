534e559ae88a031ad749d41c19a4a2d25fc89248
Kawsar Bin Siraj <38612699+kawsarBinSiraj@users.noreply.github.com>
2022-08-03 13:01:49 +0600

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppHelmet from '../components/AppLayouts/AppHelmet/AppHelmet';
+import { ThemeSettings as TSettings } from '../components/AppModules/GlobalSettings/ThemeSettings';
 
 const Dashboard = () => {
 	return (
 		<>
 			<AppHelmet title="Dashboard" />
-			<h1 className="display-5 text-theme-color">Welcome to API ERP !</h1>
+			<h1 className="display-5 text-theme-color mb-3">Hello World !</h1>
+			<div className="row">
+				<div className="col-xxl-5 col-xl-6 col-lg-8">
+					<TSettings />
+				</div>
+			</div>
 		</>
 	);
 };
